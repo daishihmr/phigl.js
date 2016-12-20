e53942ba587c476acc8752270431a457b05bbe18
daishihmr <daishi.hmr@gmail.com>
2016-12-20 12:17:29 +0900

    add postprocessing

--- build/phigl.js
+++ build/phigl.js
@@ -512,6 +512,107 @@ phina.namespace(function() {
 });
 
 phina.namespace(function() {
+
+  phina.define("phigl.PostProcessing", {
+
+    gl: null,
+    drawer: null,
+
+    width: 0,
+    height: 0,
+
+    init: function(gl, shader, uniforms, width, height) {
+      this.gl = gl;
+
+      if (typeof(shader) == "string") {
+        shader = phigl.PostProcessing.createProgram(shader);
+      }
+      width = width || 256;
+      height = height || 256;
+      uniforms = uniforms || [];
+
+      var sqWidth = Math.pow(2, Math.ceil(Math.log2(width)));
+      var sqHeight = Math.pow(2, Math.ceil(Math.log2(height)));
+
+      this.drawer = phigl.Drawable(gl)
+        .setDrawMode(gl.TRIANGLE_STRIP)
+        .setProgram(shader)
+        .setIndexValues([0, 1, 2, 3])
+        .setAttributes("position", "uv")
+        .setAttributeData([
+          //
+          -1, +1, 0, height / sqHeight,
+          //
+          +1, +1, width / sqWidth, height / sqHeight,
+          //
+          -1, -1, 0, 0,
+          // 
+          +1, -1, width / sqWidth, 0,
+        ])
+        .setUniforms(["texture", "canvasSize"].concat(uniforms));
+
+      this.width = width;
+      this.height = height;
+      this.sqWidth = sqWidth;
+      this.sqHeight = sqHeight;
+    },
+
+    render: function(texture, uniformValues, additiveBlending) {
+      var gl = this.gl;
+
+      gl.enable(gl.BLEND);
+      gl.disable(gl.DEPTH_TEST);
+
+      if (additiveBlending) {
+        gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
+      } else {
+        gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+      }
+
+      this.drawer.uniforms.texture.setValue(0).setTexture(texture);
+      this.drawer.uniforms.canvasSize.value = [this.sqWidth, this.sqHeight];
+      if (uniformValues) this.setUniforms(uniformValues);
+      this.drawer.draw();
+
+      return this;
+    },
+
+    setUniforms: function(uniformValues) {
+      var uniforms = this.drawer.uniforms;
+      uniformValues.forIn(function(k, v) {
+        uniforms[k].value = v;
+      });
+    },
+
+    _static: {
+      vertexShaderSource: [
+        "attribute vec2 position;",
+        "attribute vec2 uv;",
+
+        "varying vec2 vUv;",
+
+        "void main(void) {",
+        "  vUv = uv;",
+        "  gl_Position = vec4(position, 0.0, 1.0);",
+        "}",
+      ].join("\n"),
+
+      createProgram: function(fragmentShader) {
+        var vertexShader = phigl.VertexShader();
+        vertexShader.data = this.vertexShaderSource;
+
+        return phigl.Program()
+          .attach(vs)
+          .attach(fragmentShader)
+          .link();
+      },
+    },
+
+  });
+
+});
+
+phina.namespace(function() {
   var id = 0;
 
   phina.define("phigl.Program", {
