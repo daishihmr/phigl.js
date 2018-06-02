f24c1c971dfe44364f7f2709501663cfbef5d6bd
daishihmr <daishi.hmr@gmail.com>
2018-06-03 07:24:16 +0900

    add WebGLCanvasPool

--- build/phigl.js
+++ build/phigl.js
@@ -1668,4 +1668,49 @@ phina.namespace(function() {
 
   });
 });
+phina.namespace(function() {
+
+  phina.define("phigl.WebGLCanvasPool", {
+
+    _pool: null,
+    _actives: null,
+
+    init: function() {
+      this._pool = [];
+      this._actives = [];
+    },
+
+    create: function() {
+      const canvas = document.createElement("canvas");
+      const gl = canvas.getContext("webgl");
+      canvas.webglId = phigl.GL.getId(gl);
+      return canvas;
+    },
+
+    get: function() {
+      if (this._pool.length === 0) {
+        const canvas = this.create();
+        const self = this;
+        cavnas.relase = function() {
+          self.dispose(this);
+        };
+        this._pool.push(canvas);
+      }
+
+      const result = this._pool.shift();
+      this._actives.push(result);
+      return result;
+    },
+
+    dispose: function(canvas) {
+      if (this._actives.indexOf(canvas)) {
+        this._actives.erase(canvas);
+        this._pool.push(canvas);
+      }
+    },
+
+  });
+
+});
+
 //# sourceMappingURL=phigl.js.map
